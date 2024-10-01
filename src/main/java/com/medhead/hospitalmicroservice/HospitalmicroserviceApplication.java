a4ec886fd8d26a7be864a85ca5c107f5371dd0d3
ludo77100 <ludovic.s@protonmail.com>
2024-10-01 10:11:55 +0200

    First commit

---
+++ src/main/java/com/medhead/hospitalmicroservice/HospitalmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.hospitalmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalmicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medhead/hospitalmicroservice/HospitalmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medhead.hospitalmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
